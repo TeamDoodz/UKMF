3a386f6ddab8028368f6e4fbbb1bf7bccac0a2da
TeamDoodz <unconfigured@null.spigotmc.org>
2022-07-27 17:43:13 -0500

    remove commented out code

--- UKMF/CustomSandboxTools.cs
+++ UKMF/CustomSandboxTools.cs
@@ -8,25 +8,6 @@ namespace UKMF {
 	/// Allows mods to add their own tools to the spawn menu.
 	/// </summary>
 	public static class CustomSandboxTools {
-		/*
-		private static List<CheatAssetObject.KeyIcon> customIcons = new List<CheatAssetObject.KeyIcon>();
-		/// <summary>
-		/// Adds a new sandbox icon. Make sure the name of the icon is unique; you can do this by adding your mod GUID as a prefix to the name.
-		/// </summary>
-		public static void AddNewIcon(CheatAssetObject.KeyIcon icon) {
-			if(customIcons.Any((x) => x.key == icon.key)) {
-				MainPlugin.logger.LogWarning($"Found other modded sandbox icons with the same name as \"{icon.key}\"; they will be overwritten. If this was intentional, you can ignore this warning.");
-				customIcons.RemoveAll((x) => x.key == icon.key);
-			}
-			customIcons.Add(icon);
-		}
-		/// <summary>
-		/// Adds a new sandbox icon. Make sure the name of the icon is unique; you can do this by adding your mod GUID as a prefix to the name.
-		/// </summary>
-		public static void AddNewIcon(string iconName, Sprite icon) {
-			AddNewIcon(new CheatAssetObject.KeyIcon() { key = iconName, sprite = icon });
-		}
-		*/
 
 		private static List<SpawnableObject> customObjs = new List<SpawnableObject>();
 		/// <summary>
@@ -35,28 +16,6 @@ namespace UKMF {
 		public static void AddNewTool(SpawnableObject obj) {
 			customObjs.Add(obj);
 		}
-		
-		/*
-		[HarmonyPatch(typeof(IconManager))]
-		[HarmonyPatch(nameof(IconManager.CurrentIcons), MethodType.Getter)]
-		private static class AddCustomIconsPatch {
-			static bool firstTime = true;
-			static void Postfix(ref CheatAssetObject __result) {
-				if(firstTime) {
-					firstTime = false;
-					MainPlugin.logger.LogDebug("Base Game Sandbox Icons:");
-					foreach(var icon in __result.sandboxToolIcons) {
-						MainPlugin.logger.LogDebug(icon.key);
-					}
-				}
-
-				List<CheatAssetObject.KeyIcon> newIcons = new List<CheatAssetObject.KeyIcon>(__result.sandboxToolIcons);
-				newIcons.AddRange(customIcons);
-
-				__result.sandboxToolIcons = newIcons.ToArray();
-			}
-		}
-		*/
 
 		[HarmonyPatch(typeof(SpawnMenu))]
 		[HarmonyPatch(nameof(SpawnMenu.RebuildMenu))]
